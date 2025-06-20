987d25bf6d4b4163f865692aaaa7ed84db1a4d37
StanislavJS <stasyk55@gmail.com>
2025-06-20 14:41:42 +0300

    index.html

---
+++ src/task-6.ts
@@ -0,0 +1,12 @@
+function getFirstElement<T>(arr: T[]): T {
+    return arr[0];
+  }
+  
+  getFirstElement<number>([1, 2, 3]);           
+  getFirstElement<string>(["a", "b", "c"]);     
+  getFirstElement<boolean>([true, false, true]);
+  
+  console.log(`First element of numbers: ${getFirstElement<number>([1, 2, 3])}`);
+  console.log(`First element of strings: ${getFirstElement<string>(["a", "b", "c"])}`);
+  console.log(`First element of booleans: ${getFirstElement<boolean>([true, false, true])}`);
+  
